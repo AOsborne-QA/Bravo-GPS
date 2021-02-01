504f68b5210084dd3ce38461167bfc104741b9f5
mattcameron17 <mcameron@academytrainee.com>
2021-02-01 14:15:35 +0000

    Add Alert, HumidityAlert, and TempAlert

---
+++ Radar.Library/Models/Entity/HumidityAlert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radar.Library.Models.Entity
+{
+    class HumidityAlert
+    {
+        public string RetrieveHumidityAlert(Vehicle y)
+        {
+            if (y.VehicleHumidity > 60 || y.VehicleHumidity < 20)
+            {
+                // (Humidity) Alert is set to red
+                return "Red";
+            }
+            else if ((y.VehicleHumidity >= 21 && y.VehicleHumidity <= 30)
+                || (y.VehicleHumidity >= 40 && y.VehicleHumidity <= 60))
+            {
+                // (Humidity) Alert is set to amber
+                return "Amber";
+            }
+            else
+            {
+                // (Humidity) Alert is set to green
+                return "Green";
+            }
+
+        }
+    }
+}
