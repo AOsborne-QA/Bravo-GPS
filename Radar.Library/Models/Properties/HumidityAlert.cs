41f8ed3bcf8713d712505ae82102947cfe03e125
gyordanova <gyordanova@qa.com>
2021-02-02 09:40:43 +0000

    Created Properties folder

--- Radar.Library/Models/Properties/HumidityAlert.cs
+++ Radar.Library/Models/Properties/HumidityAlert.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Radar.Library.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Radar.Library.Models.Entity
+namespace Radar.Library.Models.Properties
 {
     public class HumidityAlert : Alert
     {
