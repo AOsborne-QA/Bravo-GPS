f48896096edee21e66d3e7c99714faf6264ec39d
gyordanova <gyordanova@qa.com>
2021-02-02 13:59:37 +0000

    Testing setup

--- Testing/Radar.Testing/VehicleControllerTesting.cs
+++ Testing/Radar.Testing/VehicleControllerTesting.cs
@@ -5,6 +5,7 @@ using Radar.Library;
 using Radar.Library.Interfaces;
 using Radar.Library.Models.Binding;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Radar.Testing
@@ -16,12 +17,72 @@ namespace Radar.Testing
         private VehicleController vehicleController;
         private AddVehicle addVehicle;
         private UpdateVehicle updateVehicle;
+        private Vehicle vehicle;
+        private List<Vehicle> vehicles;
+        private Mock<IVehicle> vehicleMock;
+        private List<IVehicle> vehiclesMock;
+        private Mock<IAddVehicle> addVehicleMock;
+        private Mock<IUpdateVehicle> updateVehicleMock;
+        private Mock<IVehicleViewModel> vehicleViewModelMock;
+        private List<IVehicleViewModel> vehiclesViewModelMock;
+        
+        public VehicleControllerTesting()
+        {
+            //mock setup
+            vehicleMock = new Mock<IVehicle>();
+            vehiclesMock = new List<IVehicle> { vehicleMock.Object };
+            addVehicleMock = new Mock<IAddVehicle>();
+            updateVehicleMock = new Mock<IUpdateVehicle>();
+            vehicle = new Vehicle();
+            vehicles = new List<Vehicle>();
 
+            //view models mock setup
+            vehicleViewModelMock = new Mock<IVehicleViewModel>();
+            vehiclesViewModelMock = new List<IVehicleViewModel>();
+
+            //sample models
+            addVehicle = new AddVehicle { Latitude = 10, Longitude = 10, VehicleHumidity = 10, VehicleTemp = 10 };
+            updateVehicle = new UpdateVehicle { Latitude = 12, Longitude = 10, VehicleHumidity = 10, VehicleTemp = 10 };
+
+            //controller setup
+            _logger = new Mock<ILogger<VehicleController>>();
+            repoMock = new Mock<IRepositoryWrapper>();
+            //var allVehicles = GetAll();            
+        }
+        /*private IEnumerable<Vehicle> GetVehicles()
+        {
+            var vehicles=new List<Vehicle>
+            {
+                //new Vehicle(){VehicleID=}
+            }
+        }*/
 
         [Fact]
-        public void Test1()
+        public void GetAllVehicles_Test()
         {
 
         }
+        /* public void GetAllCourses_Test()
+         {
+             //Arrange
+             mockRepo.Setup(repo => repo.Courses.FindAll()).Returns(GetCourses());
+             mockRepo.Setup(repo => repo.Registrations.FindByCondition(r => r.CourseId == It.IsAny<int>())).Returns(GetRegistrations());
+             //Act
+             var controllerActionResult = courseController.Get();
+             //Assert
+             Assert.NotNull(controllerActionResult);
+         }
+        private IEnumerable<Course> GetCourses()
+         {
+             var courses = new List<Course> {
+             new Course(){Id=1, Code="CS101", Title="Computing Basics"},
+             new Course(){Id=1, Code="CS102", Title="Computing Intermediate"}
+             };
+             return courses;
+         }
+         private Course GetCourse()
+         {
+             return GetCourses().ToList()[0];
+         }*/
     }
 }
